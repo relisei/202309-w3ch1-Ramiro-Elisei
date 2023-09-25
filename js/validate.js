39b6ee5f151ea4ba8fe4cff226f3281f8a8b4754
ramiro elisei <ramiroelisei@gmail.com>
2023-09-25 21:33:49 +0200

    Add Js

--- js/validate.js
+++ js/validate.js
@@ -1,1 +1,13 @@
+const formulario = document.querySelector("#formulario");
+const destinatario = document.querySelector(".destinatario");
+const asunto = document.querySelector(".asunto");
+const cuerpo = document.querySelector(".cuerpo");
 
+formulario.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event.target.value);
+});
+
+const validateForm = () => {};
+
+validateForm();
